e043434adc1266d244a4ae7f8d14021459440c64
nuttaaaa <122249892+nuttaaaa@users.noreply.github.com>
2024-11-10 10:30:31 -0800

    Update player.js

--- player.js
+++ player.js
@@ -25,7 +25,7 @@ const { queueNames } = require("./commands/play");
 function initializePlayer(client) {
     const nodes = [
          {
-    host: "lavalinkv4-id.serenetia.com",
+    host: "lavalink.serenetia.com",
     port: 443, 
     password: "BatuManaBisa", 
      secure: true
